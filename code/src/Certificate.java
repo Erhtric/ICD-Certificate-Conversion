dc810be42255029dd3cae0f7e54e9464e95300f8
Erhtric <erhtric@users.noreply.github.com>
2020-01-25 16:40:28 +0100

    Aggiunta classe principale!

--- code/src/Certificate.java
+++ code/src/Certificate.java
@@ -1,2 +1,6 @@
 public class Certificate {
+
+    // Costruttore
+    public Certificate() {}
+
 }
---
+++ code/src/MirabolanteConvertitoreCerificatiDiMorte.java
@@ -0,0 +1,5 @@
+public class MirabolanteConvertitoreCerificatiDiMorte {
+
+    public static void main(String[] args) {
+    }
+}
