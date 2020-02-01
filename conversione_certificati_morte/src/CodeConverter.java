e3457f99bea75537641b418440dc1da9ab98548d
StefanoTrv <stefanotravasci@gmail.com>
2020-02-01 16:38:56 +0100

    Inizio creazione di CodeConverter

--- conversione_certificati_morte/src/CodeConverter.java
+++ conversione_certificati_morte/src/CodeConverter.java
@@ -1,3 +1,14 @@
+import java.util.Map;
+
 public class CodeConverter {
+    private MapNode mappingTree;
+
+    public CodeConverter(String mapFile){
+        mappingTree=new MapNode();
+    }
+
+    public Code convert(Code code){
+        return mappingTree.findAndReturnCode(code);
+    }
 
 }
