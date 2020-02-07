7a84ae1c2164c229162f8c3644c0e1cc0c782e82
StefanoTrv <stefanotravasci@gmail.com>
2020-02-07 21:15:50 +0100

    Implementati metodi per l'introduzione di SubclassGood nell'albero della conversione
    Implementati i metodi di CodeConverter che permettono di impostare il valore di convType a SubclassGood all'interno dei nodi oppurtuno nell'albero usato per la conversione dei codici.

--- conversione_certificati_morte/src/CodeConverter.java
+++ conversione_certificati_morte/src/CodeConverter.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.Map;
 
 public class CodeConverter {
@@ -8,7 +9,7 @@ public class CodeConverter {
         mappingTree=new MapNode();
         useSubclassGood=false;
         fillTreeFromFile(mapFile);
-        setSubclassGoodInTree();
+        setSubclassGoodInTree(mappingTree);
     }
 
     public CodeConverter(String mapFile, boolean useSubclassGood){
@@ -16,7 +17,7 @@ public class CodeConverter {
         this.useSubclassGood=useSubclassGood;
         fillTreeFromFile(mapFile);
         if(useSubclassGood){
-            setSubclassGoodInTree();
+            setSubclassGoodInTree(mappingTree);
         }
     }
 
@@ -24,8 +25,32 @@ public class CodeConverter {
 
     }
 
-    private void setSubclassGoodInTree(){
+    //Ricevuto MapNode, cambia convType dei Code di tutti i suoi nodi da Subclass a SubclassGood
+    private void setSubclassGoodInTree(MapNode tree){
+        if(tree.getNodeCode()!=null){
+            if(tree.getNodeCode().getConvType()==Code.ConversionType.Subclass&&!multipleICD11InTree(tree.getNodeCode(),mappingTree)){
+                tree.getNodeCode().setConversionTypeToSubclassGood();
+            }
+        }
+        Iterator iterator= tree.childrenIterator();
+        while (iterator.hasNext()){
+            setSubclassGoodInTree((MapNode) iterator.next());
+        }
+    }
 
+    //ritorna vero se trova lo stesso valore ICD11 di code nell'albero in un nodo diverso da quello di code.
+    private boolean multipleICD11InTree(Code code, MapNode tree){
+        if(tree.getNodeCode()!=null&&tree.getNodeCode().getIcd11Code().equals(code.getIcd11Code())&&tree.getNodeCode()!=code){
+            return true;
+        }else{
+            Iterator iterator= tree.childrenIterator();
+            while (iterator.hasNext()){
+                if(multipleICD11InTree(code,(MapNode) iterator.next())){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     public Code convert(Code code){
